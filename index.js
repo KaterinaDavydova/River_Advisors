4c165b0167916e86dfd7e54608fd7c6bd65ead85
Kateryna Davydova <katerina.davydova.02@gmail.com>
2023-08-24 15:13:24 +0300

    final version

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
-let menuBtn = document.querySelector('.burger-btn');
-let menu = document.querySelector('.top__items');
+let menuBtn = document.querySelector('.top__logo');
+let menu = document.querySelector('.top__items_menu');
 
 menuBtn.addEventListener('click', function (event) {
     event.preventDefault();
     menuBtn.classList.toggle('active');
     menu.classList.toggle('active');
+    document.body.classList.toggle('lock')
 })
+
+menu.onclick = () => {
+    if (menu.classList.contains('active')) {
+        menuBtn.classList.remove('active');
+        menu.classList.remove('active');
+        document.body.classList.remove('lock')
+    }
+}
+
